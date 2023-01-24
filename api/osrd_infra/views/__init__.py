9ce1ab9116b03066ff918ad9a3322949a47ab987
Baptiste Prevot <baptiste.prevot@student-cs.fr>
2023-01-24 13:50:26 +0100

    api: add get electrical profiles endpoint

--- api/osrd_infra/views/__init__.py
+++ api/osrd_infra/views/__init__.py
@@ -1,6 +1,7 @@
 from osrd_infra.views.health import *  # noqa
 from osrd_infra.views.infra import *  # noqa
 from osrd_infra.views.pathfinding import *  # noqa
+from osrd_infra.views.electrical_profiles import *  # noqa
 from osrd_infra.views.rolling_stock import *  # noqa
 from osrd_infra.views.timetable import *  # noqa
 from osrd_infra.views.version import *  # noqa
